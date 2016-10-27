c503a2d1fd7c725fb3dd3627e75e57ed214e1dba
Saurabh Sood <saurabhsood91@gmail.com>
2016-10-27 10:00:56 -0700

    update karma

--- webapp/webapp/static/karma.conf.js
+++ webapp/webapp/static/karma.conf.js
@@ -1,59 +1,40 @@
-// module.exports = function(config) {
-//   config.set({
-//     frameworks: ['jasmine'],
-//
-//     files: [
-//       'js/test_*.js'
-//     ]
-//   })
-// }
-
 module.exports = function(config) {
 
-    // var configuration = {
-    //     // other things
-    //
-    //     customLaunchers: {
-    //         Chrome_travis_ci: {
-    //             base: 'Chrome',
-    //             flags: ['--no-sandbox']
-    //         }
-    //     },
-    // };
-    //
-    // if (process.env.TRAVIS) {
-    //     configuration.browsers = ['Chrome_travis_ci'];
-    // } else {
-    //     configuration.browsers = ['PhantomJS'];
-    // }
-    //
-    // config.set(configuration);
+var configuration = {
+  basePath: '',
+  frameworks: ['jasmine'],
+  files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'js/*.js'
+  ],
+  exclude: [
+  ],
+  preprocessors: {
+  },
+  reporters: ['progress'],
+  port: 9876,
+  colors: true,
+  logLevel: config.LOG_INFO,
+  autoWatch: true,
+  customLaunchers: {
+    Chrome_travis_ci: {
+      base: 'Chrome',
+      flags: ['--no-sandbox']
+    }
+  },
+  browsers: ['Chrome'],
+  plugins: [
+      'karma-jasmine',
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher'
+  ],
+  concurrency: Infinity
+};
 
-
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine'],
-    files: [
-        'node_modules/angular/angular.js',
-        'node_modules/angular-mocks/angular-mocks.js',
-        'js/*.js'
-    ],
-    exclude: [
-    ],
-    preprocessors: {
-    },
-    reporters: ['progress'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    plugins: [
-        'karma-jasmine',
-        'karma-phantomjs-launcher',
-        'karma-chrome-launcher'
-    ],
-    singleRun: true,
-    concurrency: Infinity
-  })
+    if(process.env.TRAVIS){
+      configuration.browsers = ['Chrome_travis_ci'];
+      configuration.singleRun = true;
+    }
+    config.set(configuration);
 }
